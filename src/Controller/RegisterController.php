4da65a8c2ad39a52228ef526712773a285603102
DimitriNice <web.dimitri.nice@gmail.com>
2023-10-11 07:33:18 +0200

    fin de password hashé

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,10 +8,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
-
-
 
 class RegisterController extends AbstractController
 {
@@ -22,7 +20,7 @@ class RegisterController extends AbstractController
     }
 
     #[Route('/inscription', name: 'register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -31,6 +29,11 @@ class RegisterController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+            
+            $plaintextPassword = $user -> getPassword();
+            $hashedPassword = $passwordHasher -> hashPassword($user, $plaintextPassword);
+            $user -> setPassword($hashedPassword);
+
             
             $this->entityManager->persist($user);
             $this->entityManager->flush();
